9806df41fe72caf472e4e61092a0d9c9f9637f53
Harshil Mer <75804109+harshil2002@users.noreply.github.com>
2022-05-03 10:39:46 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,33 @@
+
+export const addFooter = async () =>{
+    const footer = `
+    <h3>Explore the BBC</h3>
+    <div id = "id1">
+    <ul>
+        <li><a href="home.html">Home</a></li>
+        <li><a href="travel.html">Travel</a></li>
+        <li><a href="entertainment.html">Entertainment</a></li>
+        <li><a href="health.html">Health</a></li>
+        <li><a href="science.html">Science</a></li>
+        <li><a href="sports.html">Sports</a></li>
+        <li><a href="Technology.html">Technology</a></li>
+    </ul>
+    <hr/>
+    <ul>
+        <li><a href="home.html">Terms of Use</a></li>
+        <li><a href="home.html">About the BBC</a></li>
+        <li><a href="home.html">Privacy Policy</a></li>
+        <li><a href="home.html">Cookies</a></li>
+        <li><a href="home.html">Accessibility Help</a></li>
+        <li><a href="home.html">Parental Guidence</a></li>
+        <li><a href="home.html">Contact the BBC</a></li>
+        <li><a href="home.html">Get Personalised NewsLetters</a></li>
+    </ul>
+    <p>Copyright © 2022 BBC. The BBC is not responsible for the content of external sites. Read about our approach to external linking.</p>
+    
+</div>`;
+
+
+
+return footer;
+}
